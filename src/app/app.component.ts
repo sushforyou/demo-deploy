7eeb5987c8e0c1c26c413044f2ec5e9f4e2342b7
Sushant <kunkekarsushant@gmail.com>
2021-02-04 21:08:48 +0530

    changed boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-deploy';
+  title = 'SpaceX Launch Program';
+  developedBy = 'Sushant Kunkekar';
 }
