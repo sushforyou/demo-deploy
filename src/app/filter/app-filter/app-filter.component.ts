5fd6b5ad2e50f1b1252966a35ccca3042d72cba5
Sushant <kunkekarsushant@gmail.com>
2021-02-07 00:27:30 +0530

    Code changed in filter componet

--- src/app/filter/app-filter/app-filter.component.ts
+++ src/app/filter/app-filter/app-filter.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-filter.component.scss']
 })
 export class AppFilterComponent implements OnInit {
-
+   public yearArr = [ 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
+                      2014, 2015, 2016, 2017, 2018, 2019, 2020];
   constructor() { }
-
   ngOnInit() {
   }
 
