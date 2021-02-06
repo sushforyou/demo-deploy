abcebca54a9f4057946a4d1703d68823829dd748
Sushant <kunkekarsushant@gmail.com>
2021-02-06 09:43:22 +0530

    responsiveness to mobile devies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SpaceX Launch Program';
+export class AppComponent implements AfterViewInit {
+  title = 'SpaceX Launch Programs';
   developedBy = 'Sushant Kunkekar';
-}
+  constructor(private elementRef: ElementRef) { }
+       ngAfterViewInit() {
+         this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#eaebed';
+      }
+ }
