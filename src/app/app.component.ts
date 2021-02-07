6eea355e104128ef27807d5bbf727e182e564455
Sushant <kunkekarsushant@gmail.com>
2021-02-07 23:07:08 +0530

    all service integration and databinding from response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { AppService } from './app.service';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,41 +11,75 @@ export class AppComponent implements AfterViewInit {
   public model = { yearSelected: 0, isLaunched: false, isLanded: false };
   readonly ROOT_URL = 'https://api.spaceXdata.com/v3/launches?limit=100';
   public posts: any;
+  public imgUrl = '';
+  public missionName = '';
+  public flightNumber = '';
+  public missionId = [];
+  public launchYear ;
+  public launchSuccess;
+  public landSuccess;
   constructor(
     public service: AppService,
     private http: HttpClient,
     private elementRef: ElementRef
-  ) {}
+  ) {
+    this.getData();
+  }
   ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
-      '#eaebed';
+     '#eaebed';
   }
   public fiteredData(data) {
     this.model = data;
-    console.log(this.model.yearSelected);
-    console.log(this.model.isLaunched);
-    console.log(this.model.isLanded);
-    const launch = this.model.isLaunched;
-    this.launchData(launch);
+    const islaunch = this.model.isLaunched;
+    const island = this.model.isLanded;
+    const year = this.model.yearSelected;
+    if (islaunch && island !== undefined && year) {
+      this.allData(islaunch, island, year);
+    } else if (islaunch && island !== undefined) {
+      this.landData(islaunch, island);
+    } else if (islaunch !== undefined) {
+      this.launchData(islaunch);
+    }
   }
+  // API end point for the first-time page load without any Filters:
   public getData() {
     this.service.getDefaultData().subscribe((data: any) => {
       this.posts = data;
+      for ( const obj of this.posts) {
+        this.missionName = obj.mission_name;
+        this.flightNumber = obj.flight_number;
+        this.launchYear = obj.launch_year;
+        this.launchSuccess = obj.launch_success;
+        this.landSuccess = obj.launch_landing;
+        this.imgUrl = obj.links.mission_patch_small;
+      }
     });
   }
-  public launchData(l) {
-    const launch = l;
-    this.service.launchData(launch).subscribe((data: any) => {
+  // Launch Success Filter:
+  public launchData(lanuchValue) {
+    const launch = lanuchValue;
+    this.service.launchData(launch).subscribe(
+      (data: any) => {
+        this.posts = data;
+      },
+      err => console.log(err)
+    );
+  }
+  // Launch & Land Filter:
+  public landData(lanuchValue, landValue) {
+    const launch = lanuchValue;
+    const land = landValue;
+    this.service.landData(launch, land).subscribe((data: any) => {
       this.posts = data;
     });
   }
-  public landData() {
-    this.service.landData().subscribe((data: any) => {
-      this.posts = data;
-    });
-  }
-  public allData() {
-    this.service.allData().subscribe((data: any) => {
+  // All data with year, lauch and land filter
+  public allData(lanuchValue, landValue, yearValue) {
+    const launch = lanuchValue;
+    const land = landValue;
+    const year = yearValue;
+    this.service.allData(launch, land, year).subscribe((data: any) => {
       this.posts = data;
     });
   }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +8,43 @@ export class AppService {
   readonly ROOT_URL = 'https://api.spaceXdata.com/v3/launches?limit=100';
   public posts: any;
   constructor(private http: HttpClient) {}
-
-  public getString() {
-    const str = 'dummy  string';
-    return str;
-  }
+  // Service for default page load
   public getDefaultData() {
     return this.http.get(this.ROOT_URL);
   }
+  // Launch success filter
   public launchData(launch) {
-    return this.http.get(this.ROOT_URL + 'launch_success=' + launch);
+    let param = new HttpParams();
+    param = param.set('launch_success', launch);
+    return this.http.get(this.ROOT_URL + '&launch_success=' + launch);
   }
-  public landData() {
-    return this.http.get(this.ROOT_URL);
+  // Launch and land filter
+  public landData(launchValue, landValue) {
+    let param = new HttpParams();
+    param = param.set('launch_success', launchValue);
+    param = param.set('land_success', landValue);
+    return this.http.get(
+      this.ROOT_URL +
+        '&launch_success=' +
+        launchValue +
+        '&land_success=' +
+        landValue
+    );
   }
-  public allData() {
-    return this.http.get(this.ROOT_URL);
+  // Fiter for all
+  public allData(launchValue, landValue, yearValue) {
+    let param = new HttpParams();
+    param = param.set('launch_success', launchValue);
+    param = param.set('land_success', landValue);
+    param = param.set('launch_year', yearValue);
+    return this.http.get(
+      this.ROOT_URL +
+        '&launch_success=' +
+      launchValue +
+        '&land_success=' +
+        landValue +
+        '&launch_year=' +
+      yearValue
+    );
   }
 }
